6630378b064c6d9a398f58b40a0e8598c5ec2ccc
Talha-Bin-Sajid <148731484+Talha-Bin-Sajid@users.noreply.github.com>
2023-10-25 00:42:47 +0500

    Add files via upload

---
+++ LAB 1/Question # 2.c
@@ -0,0 +1,16 @@
+/*#include <stdio.h>
+#include <stdlib.h>
+//QUES 2
+
+
+int main()
+{
+    float height,width;
+    printf("Enter height of Rectangle :");
+    scanf("%f",&height);
+    printf("Enter width of Rectangle :");
+    scanf("%f",&width);
+    printf("Perimeter of Rectangle = %.2f UNITS\n",(height+width)*2);
+    printf("Area of Rectangle = %.2f SQUARE UNITS",height*width);
+    return 0;
+}*/
